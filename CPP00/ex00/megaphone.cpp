c2750fc2c09e0cb9bdf673c8d866578e63482ec7
Oussama Lamrabti <olamrabt@e2r8p13.1337.ma>
2024-10-04 18:33:33 +0100

    solved ex00

---
+++ CPP00/ex00/megaphone.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main(int ac, char *av[])
+{
+    int i = 1;
+    if (ac > 2)
+    {
+        while (i < ac)
+        {
+            std::string arg = av[i];
+            for (std::size_t j = 0; j < arg.length(); j++)
+                std ::cout << static_cast<char>(toupper(arg[j]));
+            i++;
+        }
+    }
+    else
+        std ::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    std ::cout << std ::endl, 0;
+    return 0;
+}
